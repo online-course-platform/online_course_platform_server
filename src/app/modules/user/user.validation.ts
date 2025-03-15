ccaa96102e4abdf4273fa69c11081c79d413f40f
Md Moniruzzaman <mdmoniruzzamanshuvo2003@gmail.com>
2025-03-15 21:07:19 +0600

    send all data to organize repo

--- src/app/modules/user/user.validation.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-const zodValidationSchema = z.object({
-  id: z.string().nonempty('ID is required'),
-  password: z.string().nonempty('Password is required').optional(),
-});
-
-export default zodValidationSchema;
